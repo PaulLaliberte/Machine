71acb601608930b2d5e65c8dd2ba9e1c018103d1
Paul Laliberte <pala5552@colorado.edu>
2017-01-29 00:37:18 -0700

    o

--- csci5622/homework/knn/knn.py
+++ csci5622/homework/knn/knn.py
@@ -84,7 +84,6 @@ class Knearest:
                 break
 
         if tie_counter > 1:
-            print 'here'
             return np.median(values_array)
         else:
             return tup_key_vals[len(tup_key_vals)-1][0]
